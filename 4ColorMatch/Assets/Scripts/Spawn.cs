366ffc85797ca7a7040277e83204c683662819d7
prafulRb <42999689+prafulRb@users.noreply.github.com>
2019-02-21 01:50:53 +0545

    Added Spawn Script to spawn different colors of ball at different 3 positions randomly.

---
+++ 4ColorMatch/Assets/Scripts/Spawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+	public GameObject[] ballObj;
+	public Transform[] spawnPos;
+	float newTime;
+	public float spawnTime;
+	public float minSpawnTime;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((Time.time - newTime) > spawnTime) {
+			Instantiate (ballObj [Random.Range(0, ballObj.Length)], spawnPos [Random.Range(0, spawnPos.Length)].position, Quaternion.identity);
+			if (spawnTime > minSpawnTime) {
+				spawnTime -= 0.1f;
+			}
+			newTime = Time.time;
+		}
+	}
+}
