c3b7541f0f22b79a7c637af4211fd98e98137f2a
prafulRb <42999689+prafulRb@users.noreply.github.com>
2019-02-21 00:16:53 +0545

    Now only rectangle object out of 3 which is clicked will only be rotated

--- 4ColorMatch/Assets/Scripts/GameController.cs
+++ 4ColorMatch/Assets/Scripts/GameController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
-	public float finalRotationZ;
-	public Vector3 currentRotation;
+	public float[] finalRotationZ;
+	public Vector3[] currentRotation;
 	public float speed;
+	int objNum;
+	public GameObject[] RectangleObj;
 	// Use this for initialization
 	void Start () {
-		finalRotationZ = transform.eulerAngles.z;
+		finalRotationZ = new float[3];
+		currentRotation = new Vector3[3];
 	}
 	
 	// Update is called once per frame
@@ -17,34 +20,46 @@ public class GameController : MonoBehaviour {
 			
 			RaycastHit2D hit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint (Input.mousePosition), Vector2.zero);
 			if (hit.collider != null) {
-				currentRotation = transform.eulerAngles;
-				if (((finalRotationZ + 90) - currentRotation.z) < 180) {
-					finalRotationZ += 90;
-					StartCoroutine (RotateObject ());
+				if(hit.collider.tag == "Red" || hit.collider.tag == "Green" || hit.collider.tag == "Blue" || hit.collider.tag == "Yellow"){
+
+					Debug.Log("Working");
+					if (hit.collider.gameObject.transform.parent.name == "Rectangle1") {
+						objNum = 0;
+					}else if (hit.collider.gameObject.transform.parent.name == "Rectangle2") {
+						objNum = 1;
+					}else if (hit.collider.gameObject.transform.parent.name == "Rectangle3") {
+						objNum = 2;
+					}
+					currentRotation [objNum] = RectangleObj [objNum].transform.eulerAngles;
+					if (((finalRotationZ[objNum] + 90) - currentRotation[objNum].z) < 180) {
+						finalRotationZ[objNum] += 90;
+						StartCoroutine (RotateObject (objNum));
+					}
 				}
-
 			}
 		}
 
 	}
 
-	IEnumerator RotateObject(){
+
+	IEnumerator RotateObject(int num){
 		yield return new WaitForSeconds (0);
-		currentRotation += new Vector3(0,0,speed*Time.deltaTime);
+		currentRotation[num] += new Vector3(0,0,speed*Time.deltaTime);
 
-		if (currentRotation.z < finalRotationZ) {
-			transform.eulerAngles = currentRotation;
-			StartCoroutine (RotateObject ());
+		if (currentRotation[num].z < finalRotationZ[num]) {
+			RectangleObj[num].transform.eulerAngles = currentRotation[num];
+			StartCoroutine (RotateObject (num));
 		} else {
-				transform.eulerAngles = new Vector3 (0, 0, finalRotationZ);
-			if (finalRotationZ == 360) {
-				transform.eulerAngles = new Vector3 (0, 0, 0);
-				finalRotationZ -= 360;
-			} else if (finalRotationZ > 360) {
-				finalRotationZ -= 360;
+			RectangleObj[num].transform.eulerAngles = new Vector3 (0, 0, finalRotationZ[num]);
+			if (finalRotationZ[num] == 360) {
+				RectangleObj[num].transform.eulerAngles = new Vector3 (0, 0, 0);
+				finalRotationZ[num] -= 360;
+			} else if (finalRotationZ[num] > 360) {
+				finalRotationZ[num] -= 360;
 			}
 		}
 	}
+
 
 
 
