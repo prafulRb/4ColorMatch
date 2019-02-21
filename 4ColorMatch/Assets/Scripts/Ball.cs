ad3bf9861a8451b77eff85fdc9ad4092853d498a
prafulRb <42999689+prafulRb@users.noreply.github.com>
2019-02-21 16:40:49 +0545

    Used Trigger to check weather ball has hit object with color same as ball

--- 4ColorMatch/Assets/Scripts/Ball.cs
+++ 4ColorMatch/Assets/Scripts/Ball.cs
@@ -5,13 +5,26 @@ using UnityEngine;
 public class Ball : MonoBehaviour {
 	public float speed;
 	public float dir;
+
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.position += new Vector3 (0, speed * dir*Time.deltaTime, 0);
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		
+		if (col.gameObject.tag == this.gameObject.tag) {
+			Destroy (this.gameObject);
+
+		} else {
+			//GameOver
+			Debug.Log("GameOver");
+
+		}
 	}
 }
