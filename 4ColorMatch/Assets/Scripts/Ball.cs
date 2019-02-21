31b774ad7c4b8febf9c37060477b4ecd7f64f464
prafulRb <42999689+prafulRb@users.noreply.github.com>
2019-02-21 14:35:18 +0545

    Added Ball Script with basic movement of ball falling down

---
+++ 4ColorMatch/Assets/Scripts/Ball.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+	public float speed;
+	public float dir;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3 (0, speed * dir*Time.deltaTime, 0);
+	}
+}
