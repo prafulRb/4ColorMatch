7888254d725f399613cb2c99f174ac35a88d403b
prafulRb <42999689+prafulRb@users.noreply.github.com>
2019-05-30 00:48:12 +0545

    Moved gameover and score logic from Ball Script to GameOverAndScore Script

--- 4ColorMatch/Assets/Scripts/Ball.cs
+++ 4ColorMatch/Assets/Scripts/Ball.cs
@@ -17,22 +17,5 @@ public class Ball : MonoBehaviour {
 		transform.position += new Vector3 (0, speed * dir*Time.deltaTime, 0);
 	}
 
-	void OnTriggerEnter2D(Collider2D col){
-		//If color ball lands on the same color then increase score else its gameover
-		if (col.gameObject.tag == this.gameObject.tag) {
-			GameObject.Find ("GameManager").GetComponent<ScoreAndUI> ().IncreaseScore ();
-            GameObject.Find("SoundEffect").GetComponent<Sound>().PlayEatSound();
-			Destroy (this.gameObject);
 
-		} else {
-			//GameOver
-			Debug.Log("GameOver");
-			Camera.main.GetComponent<Animator> ().SetTrigger ("shake");
-			GameObject.Find ("GameManager").GetComponent<ScoreAndUI> ().GameOver ();
-            GameObject.Find("SoundEffect").GetComponent<Sound>().PlayDieSound();
-
-            speed = 0;
-
-		}
-	}
 }
---
+++ 4ColorMatch/Assets/Scripts/GameOverAndScore.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverAndScore : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D(Collider2D col){
+		//If color ball lands on the same color then increase score else its gameover
+		if (col.gameObject.tag == this.gameObject.tag) {
+			GameObject.Find ("GameManager").GetComponent<ScoreAndUI> ().IncreaseScore ();
+			GameObject.Find("SoundEffect").GetComponent<Sound>().PlayEatSound();
+			Destroy (this.gameObject);
+
+		} else {
+			//GameOver
+			Debug.Log("GameOver");
+			Camera.main.GetComponent<Animator> ().SetTrigger ("shake");
+			GameObject.Find ("GameManager").GetComponent<ScoreAndUI> ().GameOver ();
+			GameObject.Find("SoundEffect").GetComponent<Sound>().PlayDieSound();
+
+			//speed = 0;
+
+		}
+	}
+}
